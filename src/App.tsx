11c25751cbaf86ed0b3b1f61d5405379dcd7fe65
MartinGonzalez <mgonzalez@mediomelon.mx>
2023-06-17 13:36:51 -0600

    Implementacion de zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import Accordion from "./components/Accordion";
+import { useCounterStore }  from './store/counterStore.ts'
+import { shallow } from 'zustand/shallow';
 
 function App() {
+
+  const { counter, name, posts} = useCounterStore((state) => ({
+    counter: state.count,
+    name: state.title,
+    posts: state.post
+  }), shallow);
+
+  const { increment, getPosts, clearStore, multiply } = useCounterStore()
+
   return (
     <div>
       <Accordion title="Más información">
@@ -9,6 +20,30 @@ function App() {
           aspernatur.
         </p>
       </Accordion>
+
+      <h2>Name: {name}</h2>
+      <h2>Counter: {counter}</h2>
+      <button onClick={() => increment(10)}>
+        Increment by 10
+      </button>
+      <button onClick={() => getPosts()}>
+        Get posts
+      </button>
+      <hr />
+      <hr />
+      <button onClick={() => clearStore()}>
+        Clear
+      </button>
+      <hr />
+      <button onClick={() => multiply(2)}>
+        Multiply by 2
+      </button>
+      <hr />
+
+
+      {JSON.stringify(posts)}
+
+
     </div>
   );
 }
